0c410c18d5ab269e122d1b14e17aa6c8769e61b8
wulff <wulff@ratatosk.net>
2013-05-18 09:21:30 +0200

    Added support for the new credentials system in Jenkins 1.5xx

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,10 +9,19 @@
 class jenkins::config(
   $slaves = undef
 ) {
+
   file { '/var/lib/jenkins/config.xml':
     content => template('jenkins/config.erb'),
     owner => jenkins,
     group => nogroup,
     require => Package['jenkins'],
   }
+
+  file { '/var/lib/jenkins/credentials.xml':
+    source  => 'puppet:///modules/jenkins/credentials.xml',
+    owner => jenkins,
+    group => nogroup,
+    require => Package['jenkins'],
+  }
+
 }
